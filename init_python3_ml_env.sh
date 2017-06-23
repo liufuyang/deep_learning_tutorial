890706f9553c91fb06ad721f16f55c2e4e8242eb
Fuyang Liu <liufu.ty@gmail.com>
2017-06-23 22:29:43 +0200

    Add init venv script

---
+++ init_python3_ml_env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+PYTHON_ENV_NAME=p3ml-venv
+
+pip install virtualenv
+
+virtualenv -p python3 $PYTHON_ENV_NAME
+
+echo "source $(pwd)/$PYTHON_ENV_NAME/bin/activate" > .env
+
+source $(pwd)/$PYTHON_ENV_NAME/bin/activate # activate the local python environment
+
+pip install jupyter
+pip install scikit-learn
+pip install matplotlib
+pip install pandas
+pip install scipy
+pip install seaborn
+pip install graphviz
