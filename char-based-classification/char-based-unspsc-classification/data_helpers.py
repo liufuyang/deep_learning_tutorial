1c191e216917e9b15b503e4df1780d38efdab0e0
Fuyang Liu <liufu.ty@gmail.com>
2018-01-21 20:48:22 +0100

    Add char cnn unspsc classifier study - not working

---
+++ char-based-classification/char-based-unspsc-classification/data_helpers.py
@@ -0,0 +1,81 @@
+import string
+import numpy as np
+import pandas as pd
+from keras.utils.np_utils import to_categorical
+
+import re
+
+
+def clean(s):
+    """
+    Remove rare characters and change everything to lowercase
+    """
+    return re.sub(r'[^\x00-\x7f]', r'', s).lower()
+
+
+def load_ag_data():
+
+    # for train data
+    train = pd.read_csv('data/train.csv', header=None)
+    train = train.dropna()
+
+    x_train = train[0]
+    x_train = x_train.apply(clean)
+    x_train = np.array(x_train)
+
+    y_train_unspsc = train[1]
+    label_array = list(y_train_unspsc.sort_values().unique())
+    y_train = y_train_unspsc.apply(lambda c: label_array.index(c))
+    y_train = to_categorical(y_train)
+
+    # for test data
+
+    test = pd.read_csv('data/test.csv', header=None)
+    x_test = test[0]
+    x_test = x_test.apply(clean)
+    x_test = np.array(x_test)
+
+    y_test_unspsc = test[1]
+    y_test = y_test_unspsc.apply(lambda c: label_array.index(c))
+    y_test = to_categorical(y_test)
+
+    return (x_train, y_train), (x_test, y_test), label_array
+
+
+def encode_data(x, maxlen, vocab):
+    # Iterate over the loaded data and create a 1-D tensor of size maxlen
+    # In this case that will be 200. This is then placed in a 3D matrix of size
+    # data_samples x maxlen. Each character is encoded into a one-hot
+    # array. Chars not in the vocab are encoded into an all zero vector.
+
+    input_data = np.zeros((len(x), maxlen), dtype=np.int)
+    for dix, sent in enumerate(x):
+        counter = 0
+
+        for c in sent:
+            if counter >= maxlen:
+                pass
+            else:
+                ix = vocab.get(c, -1)  # get index from vocab dictionary, if not in vocab, return -1
+                input_data[dix, counter] = ix
+                counter += 1
+
+    return input_data
+
+
+def create_vocab_set():
+    # This alphabet is 69 chars vs. 70 reported in the paper since they include two
+    # '-' characters. See https://github.com/zhangxiangxiao/Crepe#issues.
+
+    alphabet = (list(string.ascii_lowercase) + list(string.digits) +
+                list(string.punctuation) + ['\n', ' '])
+    # alphabet = set(alphabet)
+    vocab_size = len(alphabet)
+
+    vocab = {} # dictionary, key as characters, values as index of a char in check
+    reverse_vocab = {}
+    for ix, t in enumerate(alphabet):
+        vocab[t] = ix
+        reverse_vocab[ix] = t
+
+    return vocab, reverse_vocab, vocab_size, alphabet
---
+++ char-based-classification/char-based-unspsc-classification/main.py
@@ -0,0 +1,66 @@
+
+import json
+import char_cnn_model
+
+import numpy as np
+import data_helpers
+np.random.seed(123)  # for reproducibility
+
+# set parameters:
+
+subset = None
+
+# Whether to save model parameters
+save = False
+model_name_path = 'data/crepe_model.json'
+model_weights_path = 'data/crepe_model_weights.h5'
+
+# Maximum length. Longer gets chopped. Shorter gets padded.
+maxlen = 200
+
+# Model params
+# Filters for conv layers
+nb_filter = [128, 256, 512, 512]
+# Conv layer kernel size
+filter_kernels = [10, 7, 3, 3]
+# Number of units in the dense layer
+dense_outputs = 1024
+
+
+# Compile/fit params
+batch_size = 80
+nb_epoch = 10
+
+print('Loading data...')
+# Expect x to be a list of sentences. Y to be a one-hot encoding of the categories.
+(xt, yt), (x_test, y_test), label_array = data_helpers.load_ag_data()
+# Number of units in the final output layer. Number of classes.
+cat_output = len(label_array)  # 1007
+
+print('Creating vocab...')
+vocab, reverse_vocab, vocab_size, alphabet = data_helpers.create_vocab_set()
+
+xt = data_helpers.encode_data(xt, maxlen, vocab)
+x_test = data_helpers.encode_data(x_test, maxlen, vocab)
+
+print('Chars vocab: {}'.format(alphabet))
+print('Chars vocab size: {}'.format(vocab_size))
+print('X_train.shape: {}'.format(xt.shape))
+
+print('Build model...')
+
+model = char_cnn_model.model(filter_kernels, dense_outputs, maxlen, vocab_size, nb_filter, cat_output)
+
+print('Fit model...')
+model.summary()
+
+model.fit(xt, yt,
+          validation_data=(x_test, y_test), batch_size=batch_size, epochs=nb_epoch, shuffle=True)
+
+if save:
+    print('Saving model params...')
+    json_string = model.to_json()
+    with open(model_name_path, 'w') as f:
+        json.dump(json_string, f)
+
+    model.save_weights(model_weights_path)
